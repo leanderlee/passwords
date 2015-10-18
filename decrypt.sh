db0d483e7ef3c0c028513931d1c9d28ecb54baa3
Leander (Macbook) <me@leander.ca>
2015-10-18 02:32:21 -0700

    Added .gitignore and made decrypt no longer leave a display file hanging around.

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,8 @@
 FILE="data";
 TMPFILE="display.txt";
 
-openssl des3 -d -in $FILE -out $TMPFILE
-less $TMPFILE
-rm $TMPFILE
+openssl des3 -d -in "$FILE" -out "$TMPFILE"
+chmod 400 "$TMPFILE"
+(sleep 0; rm -f "$TMPFILE") &
+less "$TMPFILE"
+rm -f "$TMPFILE"
